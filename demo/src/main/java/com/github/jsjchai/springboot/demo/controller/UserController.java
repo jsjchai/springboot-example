969b6cee99a429d07aa79a76a86c33cea6b1c5e1
tianhe <tianhe@icongtai.com>
2019-07-12 14:34:24 +0800

    接口配置Content-Type

--- demo/src/main/java/com/github/jsjchai/springboot/demo/controller/UserController.java
+++ demo/src/main/java/com/github/jsjchai/springboot/demo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.github.jsjchai.springboot.demo.controller;
 
 import com.github.jsjchai.springboot.demo.model.User;
+import com.github.jsjchai.springboot.demo.model.UserParam;
 import com.github.jsjchai.springboot.demo.service.UserService;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,13 +28,17 @@ public class UserController {
     }
 
     /**
-     *  	Content-Type : application/x-www-form-urlencoded  id=1 能够获取到参数  {id:"1"}无法获取到参数 @RequestParam
      *
      * @param id 用户id
      * @return  用户信息
      */
-    @PostMapping("/getById")
-    public User getById(@RequestBody Long id){
+    @PostMapping(value = "/getById",produces="application/x-www-form-urlencoded;charset=UTF-8")
+    public User getById(@RequestParam Long id){
         return userService.getById(id);
+    }
+
+    @PostMapping(value = "/getByJson",produces="application/json;charset=UTF-8")
+    public User getByJson(@RequestBody UserParam param){
+        return userService.getById(param.getId());
     }
 }
