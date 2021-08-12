09cb774a557112327214f36c7e03ee8053ab8b7e
jsjchai <jsjchai@163.com>
2021-08-12 23:15:42 +0800

    添加 监控

--- demo/src/main/java/com/jsjchai/springboot/demo/DemoApplication.java
+++ demo/src/main/java/com/jsjchai/springboot/demo/DemoApplication.java
@@ -1,13 +1,23 @@
 package com.jsjchai.springboot.demo;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
     }
 
 }
---
+++ demo/src/main/java/com/jsjchai/springboot/demo/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.jsjchai.springboot.demo.controller;
+
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+
+@RestController
+@Api(tags = {"用户"})
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private MeterRegistry registry;
+
+    @GetMapping(value = "/get")
+    @ApiOperation(value = "获取用户信息", notes = "获取用户信息")
+    public String get() {
+        Counter count = registry.counter("test_request_count", "test", "1");
+        DistributionSummary summary = registry.summary("test_amount_sum", "test", "300");
+        return "test";
+    }
+
+
+
+}
